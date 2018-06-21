659dc7051ea15623fede8f2fe544ca99a5d0d3e7
sppitts47794 <sppdevelopment47794@gmail.com>
2018-06-21 16:09:31 -0400

    Changed input from text to url

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template, url_for, request
-import requests, unicodedata
-from sumy.parsers.plaintext import PlaintextParser #We're choosing a plaintext parser here, other parsers available for HTML etc.
-from sumy.nlp.tokenizers import Tokenizer
-from sumy.summarizers.lex_rank import LexRankSummarizer #We're choosing Lexrank, other algorithms are also built in
+from __future__ import absolute_import
+from __future__ import division, print_function, unicode_literals
 
+from sumy.parsers.html import HtmlParser
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lsa import LsaSummarizer as Summarizer
+from sumy.nlp.stemmers import Stemmer
+from sumy.utils import get_stop_words
+# Imports flask modules
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
@@ -13,13 +18,20 @@ def main():
 
 @app.route('/summarized', methods=['POST'])
 def summarized():
-    content = request.form['text-field']
-    content_to_str = unicodedata.normalize('NFKD', content).encode('ascii','ignore')
-    parser = PlaintextParser.from_string(content_to_str, Tokenizer("english"))
-    summarizer = LexRankSummarizer()
-    summary = summarizer(parser.document, 5)
+    # Requests data in forms
+    URL = request.form['url-field']
+    SENTENCES_COUNT = request.form['quantity']
+    LANGUAGE = "english"
 
-    return render_template('summarized.html', summary = summary)
+    parser = HtmlParser.from_url(URL, Tokenizer(LANGUAGE))
+    stemmer = Stemmer(LANGUAGE)
+    summarizer = Summarizer(stemmer)
+    summarizer.stop_words = get_stop_words(LANGUAGE)
+    bullets = []
+    for sentence in summarizer(parser.document, SENTENCES_COUNT):
+        bullets.append(sentence)
+
+    return render_template('summarized.html', summary = bullets)
 
 if __name__ == '__main__':
     app.run(debug=True, threaded=True)
