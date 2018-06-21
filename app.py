b7d821d391b3799c9eb8694fc4da6d38f368abe1
sppitts47794 <sppdevelopment47794@gmail.com>
2018-06-21 16:44:15 -0400

    Added bs4 to get title of article

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ from sumy.nlp.stemmers import Stemmer
 from sumy.utils import get_stop_words
 # Imports flask modules
 from flask import Flask, render_template, url_for, request
+from bs4 import BeautifulSoup
+import requests, urllib2
 
 app = Flask(__name__)
 
@@ -17,21 +19,35 @@ def main():
     return render_template('index.html')
 
 @app.route('/summarized', methods=['POST'])
+def summary():
+    return render_template(
+        'summarized.html',
+        summary = summarized(),
+        header = get_title(),
+    )
+
 def summarized():
     # Requests data in forms
     URL = request.form['url-field']
     SENTENCES_COUNT = request.form['quantity']
     LANGUAGE = "english"
 
+    # Summarization
     parser = HtmlParser.from_url(URL, Tokenizer(LANGUAGE))
     stemmer = Stemmer(LANGUAGE)
     summarizer = Summarizer(stemmer)
     summarizer.stop_words = get_stop_words(LANGUAGE)
-    bullets = []
+    list = []
     for sentence in summarizer(parser.document, SENTENCES_COUNT):
-        bullets.append(sentence)
+        list.append(sentence)
 
-    return render_template('summarized.html', summary = bullets)
+    return list
+
+def get_title():
+    # Requests data from url
+    URL = request.form['url-field']
+    soup = BeautifulSoup(urllib2.urlopen(URL))
+    return soup.title.string
 
 if __name__ == '__main__':
     app.run(debug=True, threaded=True)
